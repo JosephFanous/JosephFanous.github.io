06ed519f27ca90fa8fcef439e67703e870997829
Joseph Fanous <joseph_fanous@hotmail.com>
2021-08-28 22:01:40 -0400

    init 2.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Navbar from './components/navbar';
+import Welcome from './components/welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
